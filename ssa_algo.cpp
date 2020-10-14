be8f292f17d01b118fd5b79efee5229b0057492b
Vidur Sinha <vidursinha1124@gmail.com>
2020-10-14 14:53:28 -0500

    fixed format issue

--- ssa_algo.cpp
+++ ssa_algo.cpp
@@ -1,56 +1,47 @@
-#include iostream
-#include vector
-#include string
-#include unordered_map
-#include random
-#include bitsstdc++.h 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <random>
+//#include <bits/stdc++.h> 
 using namespace std;
 
 struct Reaction {
-    vectorstring reactantTypes;
-    vectorstring productTypes;
-    vectorint reactantCoefficients;
-    vectorint productCoefficients;
-    int k; reaction constant
+    vector<string> reactantTypes;
+    vector<string> productTypes;
+    vector<int> reactantCoefficients;
+    vector<int> productCoefficients;
+    int k; //reaction constant
     bool usable = true;
     long propensity;
 };
 
 struct State {
-    unordered_mapstring, int moleculeCoefficientMap; 
+    unordered_map<string, int> moleculeCoefficientMap; 
 };
 
-
-/**Reaction reactions[2] = {
-    {{'A'}, {'B'}, {1}, {1}, 2},
-    {{'A','B'}, {'C'}, {1,1}, {1}, 3}
-};
-State initialState = {
-    {'A','B','C'}, {5, 3, 0}
-};**/
-
-void SSA(State currentState, vectorReaction reactions) {
-    cout  SSA begin  endl;
+void SSA(State currentState, vector<Reaction> reactions) {
+    cout << "SSA begin" << endl;
     bool endSimulationFlag = false;
     int iterations = 0;
     while (true) {
-         Check if the simulation is over (no reaction can take place)
-        vectorReaction validReactions;
+        // Check if the simulation is over (no reaction can take place)
+        vector<Reaction> validReactions;
         int propensitySum = 0;
-        for (auto reaction  reactions) {
+        for (auto reaction : reactions) {
             bool reactionUsable = true;
             int coefficientIndex = 0;
             int propensityProduct = reaction.k;
-            for(auto itr  reaction.reactantTypes) {
+            for(auto itr : reaction.reactantTypes) {
                 int moleculeAmount = currentState.moleculeCoefficientMap[itr];
-                propensityProduct = moleculeAmount;
+                propensityProduct *= moleculeAmount;
                 int reactantCoef = reaction.reactantCoefficients.at(coefficientIndex);
-                if (reactantCoef  moleculeAmount) {
+                if (reactantCoef > moleculeAmount) {
                     reactionUsable = false;
                 }
                 coefficientIndex++;
             }
-            reaction.usable = reactionUsable;  sets whether the reaction is usable or not at every iteration
+            reaction.usable = reactionUsable; // sets whether the reaction is usable or not at every iteration
             if(reactionUsable) {
                 reaction.propensity = propensityProduct;
                 propensitySum += propensityProduct;
@@ -60,43 +51,43 @@ void SSA(State currentState, vectorReaction reactions) {
         if (validReactions.empty()){
             break;
         }
-        vectordouble probabilityEndpoints;
+        vector<double> probabilityEndpoints;
         double probabilityPoint = 0;
-        for (auto reactionvalidReactions) {  map out number line
+        for (auto reaction:validReactions) { // map out number line
             double propensity = reaction.propensity;
-            double probability = propensity  propensitySum;
+            double probability = propensity / propensitySum;
             probabilityPoint += probability;
             probabilityEndpoints.push_back(probabilityPoint);
         }
 
-        random_device rd;  random seed
-        mt19937 gen(rd());  mersenne twister engine
-        uniform_real_distribution dis(0.0, 1.0);
+        random_device rd; // random seed
+        mt19937 gen(rd()); // mersenne twister engine
+        uniform_real_distribution<> dis(0.0, 1.0);
         double RV = dis(gen);
-        cout  Chosen U   RV  endl;
+        cout << "Chosen U: " << RV << endl;
         int chosenReactionIndex;
-        for(int i = 0; i  probabilityEndpoints.size(); i++){
-            if(RV = probabilityEndpoints[i]){
+        for(int i = 0; i < probabilityEndpoints.size(); i++){
+            if(RV <= probabilityEndpoints[i]){
                 chosenReactionIndex = i;
                 break;
             }
         }
         Reaction chosenReaction = validReactions.at(chosenReactionIndex);
         int coefficientIndex = 0;
-        for(auto itr  chosenReaction.reactantTypes) {
+        for(auto itr : chosenReaction.reactantTypes) {
             int reactantCoef = chosenReaction.reactantCoefficients.at(coefficientIndex);
-            currentState.moleculeCoefficientMap[itr] -= reactantCoef;  removes the relevant reactant from the chosen reaction from the state
+            currentState.moleculeCoefficientMap[itr] -= reactantCoef; // removes the relevant reactant from the chosen reaction from the state
             coefficientIndex++;
         }
         coefficientIndex = 0;
-        for(auto itr  chosenReaction.productTypes) {
+        for(auto itr : chosenReaction.productTypes) {
             int productCoef = chosenReaction.productCoefficients.at(coefficientIndex);
-            currentState.moleculeCoefficientMap[itr] += productCoef;  adds the relevant product from the chosen reaction from the state
+            currentState.moleculeCoefficientMap[itr] += productCoef; // adds the relevant product from the chosen reaction from the state
             coefficientIndex++;
         }
 
-        for (auto molecule  currentState.moleculeCoefficientMap){
-            cout  molecule.first     molecule.second  endl;
+        for (auto molecule : currentState.moleculeCoefficientMap) {
+            cout << molecule.first << " " << molecule.second << endl;
         }
         validReactions.clear();
         probabilityEndpoints.clear();
@@ -106,27 +97,27 @@ void SSA(State currentState, vectorReaction reactions) {
 
 int main() {
     State initialState;
-    initialState.moleculeCoefficientMap[A] = 5;
-    initialState.moleculeCoefficientMap[B] = 3;
-    initialState.moleculeCoefficientMap[C] = 0;
+    initialState.moleculeCoefficientMap["A"] = 5;
+    initialState.moleculeCoefficientMap["B"] = 3;
+    initialState.moleculeCoefficientMap["C"] = 0;
 
     Reaction reaction1;
-    reaction1.reactantTypes.push_back(A);
-    reaction1.productTypes.push_back(B);
+    reaction1.reactantTypes.push_back("A");
+    reaction1.productTypes.push_back("B");
     reaction1.reactantCoefficients.push_back(1);
     reaction1.productCoefficients.push_back(1);
     reaction1.k = 2;
 
     Reaction reaction2;
-    reaction2.reactantTypes.push_back(A);
-    reaction2.reactantTypes.push_back(B);
-    reaction2.productTypes.push_back(C);
+    reaction2.reactantTypes.push_back("A");
+    reaction2.reactantTypes.push_back("B");
+    reaction2.productTypes.push_back("C");
     reaction2.reactantCoefficients.push_back(1);
     reaction2.reactantCoefficients.push_back(1);
     reaction2.productCoefficients.push_back(1);
     reaction2.k = 3;
 
-    vectorReaction reactions;
+    vector<Reaction> reactions;
     reactions.push_back(reaction1);
     reactions.push_back(reaction2);
 
