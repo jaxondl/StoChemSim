c337b1175986e96f6b3ce65c4f334c8451316994
tallam99 <tarekyallam99@gmail.com>
2020-10-19 12:55:30 -0500

    Use vectors instead of addressing arrays directly

--- gpu_code/GPUInputParser.h
+++ gpu_code/GPUInputParser.h
@@ -3,6 +3,7 @@
 //
 #include <unordered_map>
 #include <string>
+#include <vector>
 using namespace std;
 
 #ifndef CRN_SSA_WOLFRAM_PKG_GPUINPUTPARSER_H
@@ -12,18 +13,20 @@ class GPUInputParser {
     class Reaction {
     private:
         double rrc; // rate reaction constant
-        int* update_vector; // can be added to the count vector to update it if this reaction occurs
-        int* rcoefs; // coefficients of the reactants for calculating propensity
-        string* rnames; // Keys to get the counts of the reactants.
+        vector<int> update_vector; // can be added to the count vector to update it if this reaction occurs
+        vector<int> rcoefs; // coefficients of the reactants for calculating propensity
+        vector<string> rnames; // Keys to get the counts of the reactants.
     public:
         Reaction(double rrc, int* update_vector, int* reactant_coefs, string* rnames);
         double calculate_propensity(unordered_map<string, int> species); // take the species map, reaction calculates its own propensity from it
     };
 
 private:
-    string* lines; // lines of the input text file
+    vector<string> lines; // excludes the first two lines (they are stored in num_species & num_reacts respectively)
     unordered_map<string, int*> species; // key is the species name, value is the list [index, count]
-    Reaction* reactions; // All reactions in the CRN
+    vector<Reaction> reactions; // All reactions in the CRN
+    int num_species;
+    int num_reacts;
 
     void add_reaction(double rrc, int* update_vector, int* reactant_coefs, string* names);
     void add_species(string name, int count);
@@ -31,10 +34,10 @@ public:
     GPUInputParser(); // assumes the file is called "CRN.txt" and is located in "../input/"
     GPUInputParser(string fp); // takes a string giving the location of the text file instead of assuming it's in input
     void process(); // Fills species and reactions variables by processing the input line by line
-    int* get_start_state(); // returns an array with the count of each chemical species ordered alphabetically (A-Z)
-    int* get_start_props(); // returns an array with the starting propensity of each reaction, ordered by occurence in the input file.
-    Reaction* get_reactions(); // returns an array containing all the reactions ordered by occurence in the input file.
-    int** get_state_update_matrix(); // returns a matrix of the form [[-1, 1, -1, 0][2, -2, 0, 0]...], where each row updates the species count for a given reaction
+    vector<int> get_start_state(); // returns an array with the count of each chemical species ordered alphabetically (A-Z)
+    vector<int> get_start_props(); // returns an array with the starting propensity of each reaction, ordered by occurence in the input file.
+    vector<Reaction> get_reactions(); // returns an array containing all the reactions ordered by occurence in the input file.
+    vector<vector<int>> get_state_update_matrix(); // returns a matrix of the form [[-1, 1, -1, 0][2, -2, 0, 0]...], where each row updates the species count for a given reaction
 };
 
 #endif //CRN_SSA_WOLFRAM_PKG_GPUINPUTPARSER_H
