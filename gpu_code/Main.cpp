0c4dcfe7d88357e886ce1b94e5b7df6ab0fa5e51
tallam99 <tarekyallam99@gmail.com>
2020-10-19 15:40:06 -0500

    Add main file for testing

---
+++ gpu_code/Main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Tarek on 10/19/2020.
+//
+#include <stdio.h>
+#include "GPUInputParser.h"
+
+
+int main() {
+    GPUInputParser my_parser = GPUInputParser();
+    my_parser.process();
+    return 0;
+}
