e759c045be79cde3b1a437dee6b736b03b7934df
ShibuyaSundays <Isaac.Lee@utexas.edu>
2021-01-31 18:25:20 -0600

    Allowed command line arguments (first input file path, then output file path) and ability to write to an output text file

--- CPU/main.cpp
+++ CPU/main.cpp
@@ -5,23 +5,29 @@
 #include "inputVerifier.h"
 
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <fstream> 
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
     inputVerifier *iv = new inputVerifier();
-    bool safeToRun = iv->verifyFile("C:\\Users\\ccort\\CLionProjects\\ssa-implementation\\CPU\\SampleInputs\\sample_input_SSA_file.txt");
-    //bool safeToRun = true;
+    string inputFilePath = argv[1]; // example: C:\\Users\\Isaac\\CLionProjects\\SeniorDesign\\CPU\\SampleInputs\\sample_input_SSA_file.txt
+    string outputFilePath = argv[2]; // example: C:\\Users\\Isaac\\CLionProjects\\SeniorDesign\\CPU\\output.txt
+
+cout << inputFilePath;
+
+    bool safeToRun = iv->verifyFile(inputFilePath);
     if (safeToRun) {
         // Create decoder object and get all needed data structures
         decoder *inputDecoder = new decoder();
-        //inputDecoder->decode("/Users/vidursinha/Desktop/Senior-Design/crn-ssa-wolfram-pkg/CPU/SampleInputs/sample_input_SSA_file.txt");
-        inputDecoder->decode("C:\\Users\\ccort\\CLionProjects\\ssa-implementation\\CPU\\SampleInputs\\sample_input_SSA_file.txt");
+        inputDecoder->decode(inputFilePath);
         vector<vector<pair<int, int>>> stateChangeVector = inputDecoder->getStateChangeVector();
         vector<vector<pair<int, int>>> reactantsVector = inputDecoder->getReactantVector();
         vector<double> reactionRates = inputDecoder->getkValueVector();
         vector<int> moleculeAmounts = inputDecoder->getPopulationSizes();
+        vector<string> speciesList = inputDecoder->getListOfSpecies();
         double tEnd = inputDecoder->getTEnd();
 
         // Create SSA object and begin algorithm
@@ -31,13 +37,20 @@ int main() {
         // Print the results of the SSA - all states at all times, sequentially
         vector<double> allTimes = directSSA->getAllTimes();
         vector<vector<int>> allStates = directSSA->getAllStates();
+
+        ofstream outfile;
+        outfile.open(outputFilePath);
+        outfile << "Iteration" << "\t\tTime(s) " << "\t\t\tState" << endl;
         for(int i = 0; i < allTimes.size(); i++){
-            cout << "Time: " << allTimes[i] << " State:";
-            for(int stateNum: allStates[i]){
-                cout << " " << stateNum; 
+            outfile << i << "\t\t\t\t" << setprecision(5) << scientific << allTimes[i] << "\t\t\t";
+            for(int j = 0; j < allStates[i].size(); j++){
+                outfile << speciesList[j];
+                outfile << ": " << allStates[i][j];
+                outfile << "\t";
             }
-            cout << endl;
-         }
+            outfile << endl;
+        }
+        outfile.close();
     }
     else {
         cout << "There were issue with your input file" << endl;
